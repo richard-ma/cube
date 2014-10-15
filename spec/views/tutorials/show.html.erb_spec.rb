9ef7d21d5f41f17150d2ab462cca1e7d3c1edfb1
Richard Ma <richard.ma.19850509@gmail.com>
2014-10-15 13:06:25 +0800

    fix tutorial view tests

--- spec/views/tutorials/show.html.erb_spec.rb
+++ spec/views/tutorials/show.html.erb_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "tutorials/show", :type => :view do
   before(:each) do
-    @tutorial = assign(:tutorial, Tutorial.create!(
-      :rss_feed => "Rss Feed",
-      :title => "Title"
-    ))
+    @tutorial = create :tutorial, {:title => "Title", :rss_feed => "http://rss.test/feed"}
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Rss Feed/)
+    expect(rendered).to match(/http:\/\/rss\.test\/feed/)
     expect(rendered).to match(/Title/)
   end
 end
