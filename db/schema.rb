1af7ee3634db6bcd3606fe1d7463ce82e942af6f
Richard Ma <richard.ma.19850509@gmail.com>
2014-11-21 15:06:48 +0800

    create table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141012053125) do
+ActiveRecord::Schema.define(version: 20141121070058) do
+
+  create_table "crawlers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "filters", force: true do |t|
     t.string   "filter_type"
