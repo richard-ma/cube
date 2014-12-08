7377a13924edec3f18545cca1f77e1521155daf6
Richard Ma <richard.ma.19850509@gmail.com>
2014-12-08 23:39:17 +0800

    fix #3 添加默认管理员账户 cubeadmin@cube.com - cubeadmin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Admin.create(email: 'cubeadmin@cube.com', password: 'cubeadmin', password_confirmation: 'cubeadmin')
